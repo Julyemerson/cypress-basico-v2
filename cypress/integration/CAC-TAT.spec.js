fe7e4936a6513bee6e3798ef0d8f24f74d1512fe
Julyemerson Leonizio <jb.leonizio@gmail.com>
2022-06-17 10:32:23 -0300

    first test cypress

---
+++ cypress/integration/CAC-TAT.spec.js
@@ -0,0 +1,8 @@
+/// <reference types="Cypress" />
+
+describe("Central de atendimento ao Cliente TAT", () => {
+  it("verifica o titulo da aplicação", () => {
+    cy.visit("src/index.html");
+    cy.title().should("be.equal", "Central de Atendimento ao Cliente TAT");
+  });
+});
